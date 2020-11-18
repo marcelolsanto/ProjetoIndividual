e3de133e9a2edaa4be2ed8b2f7871c7439c37e12
marcelolsanto <marcelo.lsantos@bandtec.com.br>
2020-11-18 01:54:46 -0300

    atual

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,5 @@
 function cadastrar() {
+
     var cpf = in_cpf.value;
     var usuario = in_nome.value;
     var celular = in_celular.value;
@@ -13,14 +14,15 @@ function cadastrar() {
         in_celular.style.display = 'none';
         in_cpf.style.display = 'none';
         in_senha.style.display = 'none';
-        btn_cadastro.style.display = 'none';
+        btn_cadastro.style.display = 'none'; link_cadastro
         btn_login.style.display = 'block';
         mensagem1.innerHTML = `<h3>Faça o Login!</h3>`;
         alert(`Usuário cadastrado com sucesso!`);
-        logar();
+        link_cadastro.href.innerHTML = `"/media/marcelo/HD-MARCELO/ProjetoIndividual/login.html"`;
+
     } else {
         alert(`Atenção!`);
         alert(`Preencha os dados corretamente!`);
-    }
+    };
 
-}
+};
