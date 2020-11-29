3898c597395cdc9f4de098745aed8587fc18bf13
marcelolsanto <marcelo.lsantos@bandtec.com.br>
2020-11-29 15:26:54 -0300

    Função Mostar Ocultar Texto

--- projeto-site/public/javaScript/index.js
+++ projeto-site/public/javaScript/index.js
@@ -2,16 +2,9 @@ function scroll_home() {
     var element = document.getElementById("home");
     element.scrollIntoView();
 }
-function scroll_sobre() {
-    var element = document.getElementById("sobre");
-    element.scrollIntoView();
-}
+
 function scroll_projetos() {
     var element = document.getElementById("projetos");
-    element.scrollIntoView();
-}
-function scroll_orcamento() {
-    var element = document.getElementById("orcamento");
     element.scrollIntoView();
 }
 
@@ -52,3 +45,58 @@ function ocultar_compromisso() {
     btn_compromisso2.style.display = 'none';
     btn_compromisso1.style.display = 'block';
 }
+
+var clicks = {missao:0,visao:0,valor:0}
+    function mostrar_ocultar_missao() {
+        if (clicks.missao %2 == 0) {
+            clicks.missao++;
+            mostrar_texto();
+        } else {
+            clicks.missao++;
+            ocultar_texto();
+        }
+        function mostrar_texto(){
+            h3_missao.style.display = 'block';
+            p_missao.style.display = 'block';
+        }
+        function ocultar_texto() {
+            h3_missao.style.display = 'none';
+            p_missao.style.display = 'none';
+        }
+    }
+
+    function mostrar_ocultar_visao() {
+        if (clicks.visao %2 == 0) {
+            clicks.visao++;
+            mostrar_texto();
+        } else {
+            clicks.visao++;
+            ocultar_texto();
+        }
+        function mostrar_texto(){
+            h3_visao.style.display = 'block';
+            p_visao.style.display = 'block';
+        }
+        function ocultar_texto() {
+            h3_visao.style.display = 'none';
+            p_visao.style.display = 'none';
+        }
+    }
+
+    function mostrar_ocultar_valor() {
+        if (clicks.valor %2 == 0) {
+            clicks.valor++;
+            mostrar_texto();
+        } else {
+            clicks.valor++;
+            ocultar_texto();
+        }
+        function mostrar_texto(){
+            h3_valor.style.display = 'block';
+            p_valor.style.display = 'block';
+        }
+        function ocultar_texto() {
+            h3_valor.style.display = 'none';
+            p_valor.style.display = 'none';
+        }
+    }
