5771b9a4e9880aefda0b5b3e2f500565f96fa046
unknown <marcelo.lsantos@bandtec.com.br>
2020-12-23 18:48:54 -0300

    Google Analitics

--- projeto-site/Azure/query.sql
+++ projeto-site/Azure/query.sql
@@ -6,6 +6,6 @@ create table usuario(
 	 cpf int identity(1,1) primary key,
      nome varchar(50),
      login varchar(50),
-	senha varchar(50),
-    data_usuario datetime default current_timestamp 
+	    senha varchar(50),
+        data_usuario datetime default current_timestamp 
 );
