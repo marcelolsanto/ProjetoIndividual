ac470f89c44154b69e2f22847a0948df94a26885
Marcelo Santos <marcelo.lsanto@bandtec.com.brgit config --global user.name Marcelo>
2023-03-03 15:17:46 -0300

    Correções de Login

--- projeto-site/models/usuario.js
+++ projeto-site/models/usuario.js
@@ -8,7 +8,7 @@ https://codewithhugo.com/sequelize-data-types-a-practical-guide/
 module.exports = (sequelize, DataTypes) => {
     let Usuario = sequelize.define('Usuario',{
 		id: {
-			field: 'cpf',
+			field: 'id',
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: true
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
 			allowNull: false
 		},
 		login: {
-			field: 'login',
+			field: 'email',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
