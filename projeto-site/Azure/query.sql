67d7b3ebeb7cdd55798e60861828749a56a952e3
marcelolsanto <70718093+marcelolsanto@users.noreply.github.com>
2023-03-03 11:36:55 -0300

    Update query.sql
    correção login azure

--- projeto-site/Azure/query.sql
+++ projeto-site/Azure/query.sql
@@ -3,9 +3,9 @@ select * from usuario;
 drop table usuario;
 
 create table usuario(
-	 cpf int identity(1,1) primary key,
+	 id int identity(1,1) primary key,
      nome varchar(50),
-     login varchar(50),
+     email varchar(50),
 	    senha varchar(50),
         data_usuario datetime default current_timestamp 
 );
